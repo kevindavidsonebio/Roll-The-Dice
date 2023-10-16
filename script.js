97f3624052dc0a81872d995d623136d88d5208ae
knnthkeyn <kennethd2301@gmail.com>
2023-10-16 22:53:55 +0800

    kailangan maginput ng user pagwalang input mageend

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 var firstPlayer = prompt("What is the name of the first player?");
-playerOneName = document.querySelectorAll("p")[0].innerHTML = firstPlayer;
 
-var secondPlayer = prompt("What is the name of the second player?");
-playerTwoName = document.querySelectorAll("p")[1].innerHTML = secondPlayer;
+if (firstPlayer.length <= 0) {
+  alert("Aborting! Wala kasing manalalaro. Bulok!");
+} else {
+  playerOneName = document.querySelectorAll("p")[0].innerHTML = firstPlayer;
 
-function rollDice() {
-  //Dice 1
-  var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random number 1 - 6
-  var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
-  var randomImageSource = "images/" + randomDiceImage;// images/dice1.png - images/dice6.png
-  var image1 = document.querySelectorAll("img")[0];
-  image1.setAttribute("src", randomImageSource);
+  var secondPlayer = prompt("What is the name of the second player?");
 
-  //Dice 2
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-  var image2 = document.querySelectorAll("img")[1];
-  image2.setAttribute("src", randomImageSource2);
-
-  if (randomNumber1 > randomNumber2) {
-    document.querySelector("h2").innerHTML = firstPlayer + " Wins this round!";
-  } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h2").innerHTML = secondPlayer + " Wins this round!";
+  if (secondPlayer.length <= 0) {
+    alert("Aborting! Wala kasing manalalaro. Bulok!");
   } else {
-    document.querySelector("h2").innerHTML = "Draw";
+    playerTwoName = document.querySelectorAll("p")[1].innerHTML = secondPlayer;
+    function rollDice() {
+      //Dice 1
+      var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random number 1 - 6
+      var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+      var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
+      var image1 = document.querySelectorAll("img")[0];
+      image1.setAttribute("src", randomImageSource);
+
+      //Dice 2
+      var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+      var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+      var image2 = document.querySelectorAll("img")[1];
+      image2.setAttribute("src", randomImageSource2);
+
+      if (randomNumber1 > randomNumber2) {
+        document.querySelector("h2").innerHTML =
+          firstPlayer + " Wins this round!";
+      } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h2").innerHTML =
+          secondPlayer + " Wins this round!";
+      } else {
+        document.querySelector("h2").innerHTML = "Draw";
+      }
+    }
   }
 }
-
